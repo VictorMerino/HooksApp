d57aab60bf4c78ee5da7769772fb7934797ae030
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-10-02 23:39:07 +0200

    Several counters, the ugly way

--- src/01-useState/CounterApp.tsx
+++ src/01-useState/CounterApp.tsx
@@ -1,18 +1,65 @@
 import { useState } from "react";
 
 export const CounterApp = () => {
-  const [counter, setCounter] = useState(10);
+  const [counters, setCounters] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
   return (
     <>
-      <h1>Counter: {counter}</h1>
-      <hr />
+      <h1></h1>
+      <h2>
+        Counter1: {counters.counter1}
+        <button
+          className="btn btn-secondary"
+          onClick={() =>
+            setCounters({
+              counter1: counters.counter1 + 1,
+              counter2: counters.counter2,
+              counter3: counters.counter3,
+            })
+          }
+        >
+          +1
+        </button>
+      </h2>
 
-      <button
-        className="btn btn-secondary"
-        onClick={() => setCounter(counter + 1)}
-      >
-        +1
-      </button>
+      <hr />
+      <h2>
+        Counter2: {counters.counter2}
+        <button
+          className="btn btn-secondary"
+          onClick={() =>
+            setCounters({
+              counter1: counters.counter1,
+              counter2: counters.counter2 + 1,
+              counter3: counters.counter3,
+            })
+          }
+        >
+          +1
+        </button>
+      </h2>
+
+      <hr />
+      <h2>
+        Counter3 {counters.counter3}
+        <button
+          className="btn btn-secondary"
+          onClick={() =>
+            setCounters({
+              counter1: counters.counter1,
+              counter2: counters.counter2,
+              counter3: counters.counter3 + 1,
+            })
+          }
+        >
+          +1
+        </button>
+      </h2>
+
+      <hr />
     </>
   );
 };
