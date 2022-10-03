8f75ab40243185fa357c798287189b6562eb8e52
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-10-03 14:40:18 +0200

    Add and remove event listener on useEffect method

--- src/02-useEffect/Message.tsx
+++ src/02-useEffect/Message.tsx
@@ -4,8 +4,15 @@ export const Message = () => {
   useEffect(() => {
     console.log("Message mounted");
 
+    const onMouseMove = ({ x, y }: { x: number; y: number }) => {
+      const coords = { x, y };
+      console.log(coords);
+    };
+    window.addEventListener("mousemove", onMouseMove);
     return () => {
+      // Event listener will only be triggered while component is still mounted, because we remove it when component is unmounted
       console.log("Message unmounted");
+      window.removeEventListener("mousemove", onMouseMove);
     };
   }, []);
 
