c24647bc6c7b18d8c25a7939fc3fe35516c1540e
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-11-15 17:02:51 +0100

    Add the BBQuote TS Type

--- src/03-examples/MultipleCustomHooks.tsx
+++ src/03-examples/MultipleCustomHooks.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { useFetch } from "../hooks/useFetch";
 
+type BBQuote = {
+  quoute_id: number;
+  series: string;
+  author: string;
+  quote: string;
+};
 export const MultipleCustomHooks = () => {
   const url = "https://www.breakingbadapi.com/api/quotes/1";
-  const { data, isLoading, hasError } = useFetch(url);
+  const {
+    data,
+    isLoading,
+    hasError,
+  }: { data: BBQuote[] | null; isLoading: boolean; hasError: boolean | null } =
+    useFetch(url);
   return (
     <>
       <div>Breaking Bad quotes</div>
