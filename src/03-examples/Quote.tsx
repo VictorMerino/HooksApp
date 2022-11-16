47fa6bd234fe5868f56e00efb0b92e0f97a6a2b2
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-11-16 11:03:35 +0100

    Add author

--- src/03-examples/Quote.tsx
+++ src/03-examples/Quote.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import { BBQuote } from "../types/BBQuote";
 
 export const Quote = ({ quote }: { quote: BBQuote }) => {
-  return <blockquote>{quote.quote}</blockquote>;
+  return (
+    <blockquote>
+      Quote: {quote.quote}
+      <hr />
+      Author: {quote.author}
+    </blockquote>
+  );
 };
