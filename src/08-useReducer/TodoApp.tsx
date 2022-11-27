9610459f698698dfb6f81065dbd9df60c9bce666
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-11-27 21:12:12 +0100

    Some layout with bootstrap classes

--- src/08-useReducer/TodoApp.tsx
+++ src/08-useReducer/TodoApp.tsx
@@ -18,14 +18,42 @@ export const TodoApp = () => {
   const [state, dispatch] = useReducer(todoReducer, initialState);
   return (
     <>
-      <h1>TodoApp</h1>
+      <h1>
+        TodoApp: 3 <small>(pendientes: 2)</small>
+      </h1>
       <hr />
 
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
+      <div className="row">
+        <div className="col-12 col-sm-7">
+          <ul className="list-group mb-4">
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">Item 1</span>
+              <button className="btn btn-outline-danger">-</button>
+            </li>
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">Item 2</span>
+              <button className="btn btn-outline-danger">-</button>
+            </li>
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">Item 3</span>
+              <button className="btn btn-outline-danger">-</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-12 col-sm-5">
+          <form>
+            <label>Add TO-DO</label>
+            <input
+              type="text"
+              placeholder="What to do?"
+              className="form-control"
+            />
+            <button type="submit" className="btn btn-outline-primary">
+              +
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
