7c16201f84dd5bd5190735caa9b56e6fa76d519b
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-11-27 19:56:20 +0100

    Initial functional state: with an addition action that just simply works

--- src/08-useReducer/intro-reducer.ts
+++ src/08-useReducer/intro-reducer.ts
@@ -1,4 +1,15 @@
-const initialState = [
+interface State {
+  id: number;
+  todo: string;
+  done: boolean;
+}
+
+interface ReducerAction {
+  type: string;
+  payload: State;
+}
+
+const initialState: State[] = [
   {
     id: 1,
     todo: "Do the laundry",
@@ -6,6 +17,24 @@ const initialState = [
   },
 ];
 
-const todoReducer = (state = initialState, action) => {
+const todoReducer = (state = initialState, action = "") => {
+  if (action.type === "[TODO add todo]") {
+    return [...state, action.payload];
+  }
   return state;
 };
+
+let todos = todoReducer();
+
+const newTodo: State = {
+  id: 2,
+  todo: "Go shopping",
+  done: false,
+};
+
+const addTodoAction: ReducerAction = {
+  type: "[TODO add todo]",
+  payload: newTodo,
+};
+todos = todoReducer(todos, addTodoAction);
+console.log({ state: todos });
