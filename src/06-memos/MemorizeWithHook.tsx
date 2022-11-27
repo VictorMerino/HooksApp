ae9a5680a5e7e14e76dd14809643e35af137e444
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-11-27 15:48:06 +0100

    Not optimized heavy stuff call method: each time the component re-renders the function is called again

--- src/06-memos/MemorizeWithHook.tsx
+++ src/06-memos/MemorizeWithHook.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { useCounter } from "../hooks";
 
+const heavyStuff = (iterationNumber = 100) => {
+  for (let index = 0; index < iterationNumber; index++) {
+    console.log("Lets goooo");
+  }
+  return `${iterationNumber} times`;
+};
+
 export const MemorizeWithHook = () => {
   const { counter, increment } = useCounter({ initialValue: 1 });
   const [show, setShow] = useState(true);
@@ -10,6 +17,7 @@ export const MemorizeWithHook = () => {
         Counter: <small>{counter}</small>
       </h1>
       <hr />
+      <h4> {heavyStuff(1500)}</h4>
 
       <button className="btn btn-primary" onClick={() => increment()}>
         +1
