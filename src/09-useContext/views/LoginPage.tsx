6f792ff67f8ac7206c27aa28c2ff8cac00bea11b
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-11-28 20:48:03 +0100

    useContext hook to use the Context that applies. Reaaally easy

--- src/09-useContext/views/LoginPage.tsx
+++ src/09-useContext/views/LoginPage.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/userContext";
 
 export const LoginPage = () => {
+  const { user } = useContext(UserContext);
+  console.log(user);
   return (
     <>
       <h2>LoginPage</h2>
