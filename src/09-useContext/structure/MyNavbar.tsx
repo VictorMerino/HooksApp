1d6cdb0f246e30fbfd89279a59c60fde4ad40ed0
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-11-28 14:19:04 +0100

    Fix navbar active state

--- src/09-useContext/structure/MyNavbar.tsx
+++ src/09-useContext/structure/MyNavbar.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 export const MyNavbar = () => {
-  const isItemActive = (isActive) => `nav-link ${isActive ? "active" : ""}`;
+  const isItemActive = ({ isActive }: { isActive: boolean }) =>
+    `nav-link ${isActive ? "active" : ""}`;
   return (
-    <nav className="navbar navbar-dark navbar-expand-lg">
+    <nav className="navbar navbar-light navbar-expand-lg">
       <div className="container-fluid">
         <ul className="navbar-nav">
           <NavLink className={isItemActive} to="/">
