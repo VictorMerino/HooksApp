c7a76709c4b7fc5f937f70016340c63b0f575b3c
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-12-06 22:13:21 +0100

    Add one test for each reducer action. TO-DO included with something I don't understand why is happening

--- tests/08-useReducer/todoReducer.test.ts
+++ tests/08-useReducer/todoReducer.test.ts
@@ -12,4 +12,39 @@ describe("todoReducer tests", () => {
     const newState = todoReducer(INITIAL_STATE, {});
     expect(newState).toEqual(INITIAL_STATE);
   });
+
+  it("should add a todo", () => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: {
+        id: 2,
+        description: "New TODO",
+        done: false,
+      },
+    };
+    const newState = todoReducer(INITIAL_STATE, action);
+    expect(newState.length).toEqual(2);
+    expect(newState).toContain(action.payload);
+  });
+
+  it("should remove a todo", () => {
+    const action = {
+      type: "[TODO] Remove Todo",
+      payload: 1,
+    };
+    const newState = todoReducer(INITIAL_STATE, action);
+    expect(newState.length).toEqual(0);
+  });
+
+  it("should update a todo", () => {
+    const action = {
+      type: "[TODO] Toggle Todo",
+      payload: 1,
+    };
+    const newState = todoReducer(INITIAL_STATE, action);
+    expect(newState.length).toEqual(1);
+    expect(newState[0].done).toEqual(true);
+    // TO-DO: check why INITIAL_STATE object is changing
+    expect(newState[0].done).toEqual(INITIAL_STATE[0].done);
+  });
 });
