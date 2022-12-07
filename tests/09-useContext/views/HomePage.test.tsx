b4712b97e0a08a18dbe729ba9ce1bdc874f6bd4c
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-12-07 22:39:28 +0100

    Add Context tests. NOT WORKING, failing, do not know the reason. NEED TO CHECK

---
+++ tests/09-useContext/views/HomePage.test.tsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+import { UserContext } from "../../../src/09-useContext/context/UserContext";
+import { HomePage } from "../../../src/09-useContext/views/HomePage";
+
+describe.skip("<HomePage /> tests", () => {
+  const user = { id: 1, name: "Davo", email: "davo@davo.com" };
+  it("renders page without user", () => {
+    render(
+      <UserContext.Provider value={{ user: null }}>
+        <HomePage />
+      </UserContext.Provider>
+    );
+  });
+});
---
+++ tests/09-useContext/views/LoginPage.test.tsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+import { UserContext } from "../../../src/09-useContext/context/UserContext";
+import { LoginPage } from "../../../src/09-useContext/views/LoginPage";
+
+describe.skip("<LoginPage /> tests", () => {
+  const user = { id: 1, name: "Davo", email: "davo@davo.com" };
+  it("renders page without user", () => {
+    render(
+      <UserContext.Provider value={{ user: null }}>
+        <LoginPage />
+      </UserContext.Provider>
+    );
+  });
+});
